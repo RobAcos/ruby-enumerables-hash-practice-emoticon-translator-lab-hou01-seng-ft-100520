0e023461a5b736359387ee542edfef572e3b6631
Roberto Acosta <x.rojo@hotmail.com>
2020-10-02 04:31:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+    emoticons =  YAML.load_file('emoticons.yml')
+  emoticons
+
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
+ 
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+  library = load_library(file_path)
+  if library["get_meaning"].include?(japanese_emoticon) 
+    library["get_meaning"][japanese_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
